01a7681e514e9011dbe873019b114bbd377cc64b
Unknown <sonche17@hotmail.com>
2020-05-14 12:44:17 +0200

    14-5-2020
    added checkbox so they can choose to give statements more worth
    
    added dropdown to choose whether or not to only show secular parties
    
    added dropdown so they can choose the minimal party size

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.getElementById('5').style.display = "none";
       function start() {
           var stemwijzer = document.getElementById('stemwijzer');
           document.getElementById('stemwijzer').style.display = "";
+          document.getElementById('5').style.display = "";
           updateSubject();
           document.getElementById('start').style.display = "none";
       }
@@ -14,6 +15,7 @@ document.getElementById('5').style.display = "none";
       function agreeButton(){
         var button = document.getElementById('buttonAgree');
         answer[sub] = 'pro';
+        subjects[sub].important = document.getElementById('myCheck').checked;
         sub++;
         console.log('agree');
         updateSubject();
@@ -22,6 +24,7 @@ document.getElementById('5').style.display = "none";
       function disagreeButton(){
         var button = document.getElementById('buttonDisgree');
         answer[sub] = 'contra';
+        subjects[sub].important = document.getElementById('myCheck').checked;
         sub++;
         console.log('disagree');
         updateSubject();
@@ -30,6 +33,7 @@ document.getElementById('5').style.display = "none";
       function neitherButton(){
         var button = document.getElementById('buttonNeither');
         answer[sub] = 'none';
+        subjects[sub].important = document.getElementById('myCheck').checked;
         sub++;
         console.log('neither');
         updateSubject();
@@ -37,15 +41,24 @@ document.getElementById('5').style.display = "none";
 
       function skipButton(){
         var button = document.getElementById('buttonSkip');
-        answer[sub] = '';
         sub++;
         console.log('skip');
         updateSubject();
       }
 
       function backButton(){
-        var button = document.getElementById('buttonSkip');
-        console.log('back');
+        var button = document.getElementById('5');
+        if (sub != 0) {
+          sub--;
+          console.log('back');
+          updateSubject();
+        } else {
+          var stemwijzer = document.getElementById('stemwijzer');
+          var start = document.getElementById('start');
+          stemwijzer.style.display = "none";
+          document.getElementById('5').style.display = "none";
+          start.style.display = "";
+        }
       }
 
       function updateSubject(){
@@ -53,21 +66,58 @@ document.getElementById('5').style.display = "none";
           var context = document.getElementById('statement');
           var stemwijzer = document.getElementById('stemwijzer');
           var subject = subjects[sub];
+          var checkbox = document.getElementById('myCheck');
 
           if (typeof subjects[sub] != 'undefined') {
             title.innerHTML = (sub + 1 + ". ") + subject.title;
             context.innerHTML = subject.statement;
+            document.getElementById('btns').style.display = "";
+            document.getElementById('importanceChecker').style.display = "";
+            document.getElementById('filter').style.display = "none";
           } else {
             title.innerHTML = 'resultaat';
             context.innerHTML = '';
             document.getElementById('btns').style.display = "none";
+            document.getElementById('importanceChecker').style.display = "none";
+            document.getElementById('filter').style.display = "";
             matchOpinions();
           }
+          checkbox.checked = false;
+          selectionHighlighter();
+      }
 
-          // context.innerHTML = subject.statement;
+      function selectionHighlighter () {
+        console.log('running selectionHighlighter answer = ' + answer[sub]);
+        switch (answer[sub]) {
+          case 'pro':
+          document.getElementById('1').className = 'btn btn-success';
+          document.getElementById('2').className = 'btn btn-primary';
+          document.getElementById('3').className = 'btn btn-primary';
+            break;
+          case 'contra':
+          document.getElementById('1').className = 'btn btn-primary';
+          document.getElementById('2').className = 'btn btn-primary';
+          document.getElementById('3').className = 'btn btn-success';
+            break;
+          case 'none':
+          document.getElementById('1').className = 'btn btn-primary';
+          document.getElementById('2').className = 'btn btn-success';
+          document.getElementById('3').className = 'btn btn-primary';
+            break;
+          default:
+          document.getElementById('1').className = 'btn btn-primary';
+          document.getElementById('2').className = 'btn btn-primary';
+          document.getElementById('3').className = 'btn btn-primary';
+      }
       }
 
       function matchOpinions() {
+        var partiesByOrder;
+
+
+        parties.forEach((party, index) => {
+          party.points = 0;
+        });
         // starting point are my answers
         answer.forEach((answer, answerIndex) => {
           // loop through my answers for current subject
@@ -78,23 +128,60 @@ document.getElementById('5').style.display = "none";
                 // var party = parties[getParty(subjectParty.name)];
                 var currentparty = parties[parties.findIndex(party => party.name == subjectParty.name)];
                 // add score to the party that has the same opinion
-                // party.name = 'GROENLINKS';
-                console.log(currentparty);
-                // console.log(parties[getParty(subjectParty.name)]);
-                // console.log(subjectParty.name);
+                currentparty.points++;
+                // add extra point to party if user thinks that the statement is important
+                if (subjects[answerIndex].important) {
+                  currentparty.points++;
+                  console.log(currentparty.name + 'got an extra point for subject' + answerIndex);
+                }
               }
           });
         });
+        showResult();
       }
 
-      function getParty (partyname) {
-        // loop through parties to return matching party id
-        parties.forEach((party, i) => {
+      function showResult (){
+        var container = document.createElement("OL");
+        var filterSeculier = document.getElementById('seculierinputGroupSelect01').value;
+        var filterZetels = document.getElementById('zetelGroupSelect01').value;
+        var partijen = parties;
+        document.getElementById("statement").innerHTML = '';
+        console.log('showing results');
 
-          if (party.name == partyname) {
-            console.log(i);
-
-            return i;
-          }
+        partijen.sort((a, b) => {
+          return b.points - a.points;
         });
+
+        if (filterSeculier != 'Choose...') {
+          console.log('filtering secular');
+          console.log(partijen);
+          partijen = partijen.filter(checkSeculier);
+          console.log(partijen);
+        }
+
+        if (filterZetels != 'Choose...') {
+          partijen = partijen.filter(checkZetels);
+        }
+
+        partijen.forEach((party, i) => {
+          var node = document.createElement("LI");
+          var textnode = document.createTextNode(party.name);
+          node.appendChild(textnode);
+          container.appendChild(node);
+        });
+        document.getElementById("statement").appendChild(container);
+      };
+
+      function checkSeculier(partij) {
+        if (document.getElementById('seculierinputGroupSelect01').value === 'true') {
+          var filterValue = true;
+        } else {
+          var filterValue = false;
+        }
+
+          return partij.secular === filterValue;
+      }
+
+      function checkZetels(partij) {
+        return partij.size >= document.getElementById('zetelGroupSelect01').value;
       }
